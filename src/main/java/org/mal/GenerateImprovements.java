c9c3adfcf41544aa8c8e73b24078dedb5fee8d14
maldil <malinda.dilhara@gmail.com>
2024-02-01 01:21:40 -0800

    rename class

--- src/main/java/org/mal/GenerateImprovements.java
+++ src/main/java/org/mal/GenerateImprovements.java
@@ -1,6 +1,6 @@
 package org.mal;
 
-public class Main {
+public class GenerateImprovements {
     public static void main(String[] args) {
         ProcessAST p = new ProcessAST();
         p.processProjects();
